604903c7c491643152706e50f8030c534b7a1279
DexterW1 <56940477+DexterW1@users.noreply.github.com>
2024-07-05 14:35:49 -0700

    added files

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Dashboard from "./screens/Dashboard";
+export default function Home() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
---
+++ app/screens/Dashboard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div className="container flex flex-col border border-red-500 min-h-[93vh] items-center pt-4 mx-auto">
+      <h1 className="text-4xl">Dashboard</h1>
+    </div>
+  );
+}
